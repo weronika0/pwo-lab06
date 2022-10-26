83f46b61a72fb8e1c0761fc3c90db501a3693fd3
Weronika <weronika.nowak@pollub.edu.pl>
2022-10-26 19:05:00 +0200

    Wykonanie 3 zadań z przykładowymi wynikami

--- src/main/java/pwo/seq/FibonacciGenerator.java
+++ src/main/java/pwo/seq/FibonacciGenerator.java
@@ -1,22 +1,24 @@
-package pwo;
+package pwo.seq;
 
 import java.math.BigDecimal;
 
-public class FibonacciGenerator {
+public class FibonacciGenerator extends Generator {
 
-    private int lastIndex = 0;
-    private BigDecimal current = new BigDecimal(0);
-
-    private BigDecimal f_1 = new BigDecimal(1),
-            f_2 = new BigDecimal(0);
-
-    public void reset() {
-        lastIndex = 0;
+    public FibonacciGenerator() {
         current = new BigDecimal(0);
         f_1 = new BigDecimal(1);
         f_2 = new BigDecimal(0);
     }
 
+    @Override
+    public void reset() {
+        super.reset();
+        current = new BigDecimal(0);
+        f_1 = new BigDecimal(1);
+        f_2 = new BigDecimal(0);
+    }
+
+    @Override
     public BigDecimal nextTerm() {
         if (lastIndex > 1) {
             current = f_1.add(f_2);
@@ -28,19 +30,6 @@ public class FibonacciGenerator {
             current = new BigDecimal(0);
         }
         lastIndex++;
-        return current;
-    }
-
-    public BigDecimal getTerm(int i) {
-        if (i < 0) {
-            throw new IllegalArgumentException();
-        }
-        if (i < lastIndex) {
-            reset();
-        }
-        while (lastIndex <= i) {
-            nextTerm();
-        }
         return current;
     }
 }
